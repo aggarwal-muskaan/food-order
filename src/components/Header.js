028e5f836766a84b17457c7f538ca702bf9e8471
Muskaan Aggarwal <muskaan19010@gmail.com>
2021-05-09 21:08:54 +0530

    add Header component

---
+++ src/components/Header.js
@@ -0,0 +1,26 @@
+import classes from "../styles/Header.module.css";
+import React from "react";
+import CartIcon from "../assets/CartIcon";
+import mealsImage from "../assets/meals.jpg";
+
+function Header() {
+  return (
+    <div className={classes["header-container"]}>
+      <header className={classes.header}>
+        <h1>Meals</h1>
+        <button className={classes.button}>
+          <span className={classes.icon}>
+            <CartIcon />
+          </span>
+          <span>Your Cart</span>
+          <span className={classes.badge}>5</span>
+        </button>
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={mealsImage} alt="Delicious Food!" />
+      </div>
+    </div>
+  );
+}
+
+export default Header;
