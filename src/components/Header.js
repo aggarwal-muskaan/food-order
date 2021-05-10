ca1380f41103ad2d87e6802b82577b9929b258d8
Muskaan Aggarwal <muskaan19010@gmail.com>
2021-05-10 21:02:29 +0530

    refactor : add css class conditionally to button on adding items

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,35 @@
 import classes from "../styles/Header.module.css";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { cartContext } from "../store/cart.context";
 import CartIcon from "../assets/CartIcon";
 import mealsImage from "../assets/meals.jpg";
 
 function Header({ onShowCart }) {
+  const [buttonBump, setButtonToBump] = useState(false);
   const itemsInCart = useContext(cartContext);
+
+  const btnClasses = `${classes.button} ${buttonBump ? classes.bump : ""}`;
+  useEffect(() => {
+    if (itemsInCart.length === 0) {
+      return;
+    }
+    setButtonToBump(true);
+    const timer = setTimeout(() => {
+      setButtonToBump(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [itemsInCart]);
+
   const numberofItems = itemsInCart.items.reduce((i, j) => i + j.quantity, 0);
 
   return (
     <div className={classes["header-container"]}>
       <header className={classes.header}>
         <h1>Meals</h1>
-        <button className={classes.button} onClick={onShowCart}>
+        <button className={btnClasses} onClick={onShowCart}>
           <span className={classes.icon}>
             <CartIcon />
           </span>
