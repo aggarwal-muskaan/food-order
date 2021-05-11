1a27707293f26e4a3d6b43f277d539667395db58
Muskaan Aggarwal <muskaan19010@gmail.com>
2021-05-11 20:23:06 +0530

    refactor: handled error with try-catch block
    deleted helper folder as static meals data is no more needed

--- src/components/meals/AvailableMeals.js
+++ src/components/meals/AvailableMeals.js
@@ -11,24 +11,28 @@ function AvailableMeals() {
   useEffect(() => {
     // IIFE
     (async () => {
-      setLoading(true);
-      const baseUrl =
-        "https://meals-demo-default-rtdb.firebaseio.com/DUMMY_MEALS.json";
-      const res = await axios.get(baseUrl);
-      const data = await res.data;
+      try {
+        setLoading(true);
+        const baseUrl =
+          "https://meals-demo-default-rtdb.firebaseio.com/DUMMY_MEALS.json";
+        const res = await axios.get(baseUrl);
+        const data = await res.data;
 
-      //converting Firebase object into an array
-      const mealsArray = [];
-      for (let key in data) {
-        mealsArray.push({
-          id: key,
-          name: data[key].name,
-          description: data[key].description,
-          price: data[key].price,
-        });
+        //converting Firebase object into an array
+        const mealsArray = [];
+        for (let key in data) {
+          mealsArray.push({
+            id: key,
+            name: data[key].name,
+            description: data[key].description,
+            price: data[key].price,
+          });
+        }
+        // setting state with loaded meals
+        setMeals(mealsArray);
+      } catch (error) {
+        throw new Error("Something went wrong!");
       }
-      // setting state with loaded meals
-      setMeals(mealsArray);
       setLoading(false);
     })();
   }, []);
