2c40ef0680ffee07951d6e7b1e22e1bfc4d25ca0
Muskaan Aggarwal <muskaan19010@gmail.com>
2021-05-11 19:49:09 +0530

    refactor: fetching meals from Firebase and not using static file

--- src/components/meals/AvailableMeals.js
+++ src/components/meals/AvailableMeals.js
@@ -1,15 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import classes from "../../styles/MAvailable.module.css";
 import MealItem from "./MealItem";
-import DUMMY_MEALS from "../../helpers/dummy_content";
 import MCard from "../UI/MCard";
+import axios from "axios";
 
 function AvailableMeals() {
+  const [meals, setMeals] = useState([]);
+
+  useEffect(() => {
+    // IIFE
+    (async () => {
+      const baseUrl =
+        "https://meals-demo-default-rtdb.firebaseio.com/DUMMY_MEALS.json";
+      const res = await axios.get(baseUrl);
+      const data = await res.data;
+
+      //converting Firebase object into an array
+      const mealsArray = [];
+      for (let key in data) {
+        mealsArray.push({
+          id: key,
+          name: data[key].name,
+          description: data[key].description,
+          price: data[key].price,
+        });
+      }
+      // setting state with loaded meals
+      setMeals(mealsArray);
+    })();
+  }, []);
+
   return (
     <section className={classes.meals}>
       <MCard>
         <ul>
-          {DUMMY_MEALS.map((m) => (
+          {meals.map((m) => (
             <MealItem key={m.id} {...m} />
           ))}
         </ul>
